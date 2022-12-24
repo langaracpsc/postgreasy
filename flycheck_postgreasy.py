46655100cf11682609ac9feba173acff92741397
rishit-singh <rsrishitsingh@gmail.com>
2022-12-23 23:43:46 -0800

    Added UpdateRecord() and related sub-routines

--- flycheck_postgreasy.py
+++ flycheck_postgreasy.py
@@ -16,12 +16,6 @@ class Tools:
             trimmedString += string[x]
         return trimmedString
 
-    def IsSubSet(array: list, sArray: list):
-        for val in array:
-            if (not(val in sArray)):
-                return bool(0)
-        return bool(1)
-
 class FileIO:
     def Read(path: str):
         return open(path, 'r').read()
@@ -44,7 +38,6 @@ class DBTools:
     def GetSQLFormatted(val):
         if (type(val) == str or type(val) == chr):
             return str(f"\'{val}\'")
-        return val
 
     def GetSQLValueString(tup: tuple):
         tupleString = str('(')
@@ -55,20 +48,6 @@ class DBTools:
         tupleString += f"{DBTools.GetSQLFormatted(tup[len(tup) - 1])})"
 
         return tupleString
-
-    def GetSQLSetString(recordDict: dict):
-        setString = str()
-        fields = list(recordDict.keys())
-
-        end = len(fields) - 1
-
-        for x in range(end):
-            setString += f"{fields[x]}={DBTools.GetSQLFormatted(recordDict[fields[x]])}, "
-
-        setString += f"{fields[end]}={DBTools.GetSQLFormatted(recordDict[fields[end]])}"
-
-        return setString
-
 
 
 class Database:
@@ -105,7 +84,6 @@ class Database:
         self.Cursor.close()
         self.Connection.close()
 
-
     def ExecuteCommand(self, command: str):
         result = self.Cursor.execute(command)
 
@@ -113,48 +91,17 @@ class Database:
 
         return result
 
-    def FetchQueryData(self, query: str):
-        self.Cursor.execute(query)
-
-        result = self.Cursor.fetchone()
-
-        tupleStack = list[tuple]()
-
-        while (result != None):
-            tupleStack.append(result)
-            result = self.Cursor.fetchone()
-
-        return tupleStack
-
-    def GetFieldNames(self, table: str):
-        fieldStack = list()
-        result = self.FetchQueryData(f"""
-            SELECT column_name FROM information_schema.columns WHERE table_name=\'{table}\';
-        """)
-
-        for tup in result:
-            fieldStack.append(tup[0])
-        return fieldStack
-
     def InsertRecord(self, record: tuple, table: str):
+        print(f"INSERT INTO {table} VALUES({DBTools.GetSQLValueString(record)})")
         self.ExecuteCommand(f"INSERT INTO {table} VALUES{DBTools.GetSQLValueString(record)}")
-
         return
 
-    def UpdateRecord(self, record: dict, condition: dict, table: str):
-        if (not(Tools.IsSubSet(list(record.keys()), self.GetFieldNames(table))) or not(Tools.IsSubSet(list(condition.keys()), self.GetFieldNames(table)))):
-            raise BaseException("Field names of the provided record dont match the fields of the table.")
 
-        self.ExecuteCommand(f"UPDATE {table} SET {DBTools.GetSQLSetString(record)} WHERE {DBTools.GetSQLSetString(condition)};")
 
 def Main(args: list):
     db = Database("config.json")
     db.Connect()
-    #db.InsertRecord(tuple((Tools.TrimString(str(uuid.uuid4()), 36), args[1])), args[2])
-    print(db.FetchQueryData("SELECT * FROM test;"))
-    print(db.GetFieldNames("test"))
-    db.UpdateRecord(dict({ "val": args[3] }), dict({args[1]: args[2]}), "test")
-
+    db.InsertRecord(tuple((Tools.TrimString(str(uuid.uuid4()), 36), args[1])), args[2])
     db.Disconnect()
 
 Main(sys.argv)
